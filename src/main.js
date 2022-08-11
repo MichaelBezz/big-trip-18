a8965b5a2d6e6f9b923d98de588e2c397c90d6a7
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-08-11 08:33:17 +0300

    Добавляет презентор путешествия

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { render } from './render.js';
 import FilterView from './view/filter-view.js';
+import TripPresenter from './presenter/trip-presentor.js';
 
-const header = document.querySelector('.page-header');
-const filter = header.querySelector('.trip-controls__filters');
+const filterContainer = document.querySelector('.trip-controls__filters');
+const tripContainer = document.querySelector('.trip-events');
+const tripPresenter = new TripPresenter();
 
-render(new FilterView(), filter);
+render(new FilterView(), filterContainer);
+tripPresenter.init(tripContainer);
---
+++ src/presenter/trip-presentor.js
@@ -0,0 +1,26 @@
+import { render } from '../render.js';
+import SortView from '../view/sort-view.js';
+import TripListView from '../view/trip-list-view.js';
+import TripItemView from '../view/trip-item-view.js';
+import EventEditView from '../view/event-edit-view.js';
+import EventView from '../view/event-view.js';
+
+export default class TripPresenter {
+  tripList = new TripListView();
+  tripItem = new TripItemView();
+
+  init = (tripContainer) => {
+    this.tripContainer = tripContainer;
+
+    render(new SortView(), tripContainer);
+    render(this.tripList, tripContainer);
+    render(this.tripItem, this.tripList.getElement());
+    render(new EventEditView(), this.tripItem.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      const newItem = new TripItemView();
+      render(new EventView(), newItem.getElement());
+      render(newItem, this.tripList.getElement());
+    }
+  };
+}
