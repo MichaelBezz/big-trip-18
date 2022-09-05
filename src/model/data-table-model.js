53b66eb278bc3550b626ecca165c26de701773d1
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-09-05 21:21:02 +0300

    Добавит модель фильтра и сортировки

---
+++ src/model/data-table-model.js
@@ -0,0 +1,57 @@
+import CollectionModel from './collection-model';
+
+/**
+ * Абстрактная model для фильтрации и сортировки
+ * @template Item
+ * @template {Adapter} ItemAdapter
+ * @extends CollectionModel<Item,ItemAdapter>
+ */
+export default class DataTableModel extends CollectionModel {
+
+  /** @typedef {(item: ItemAdapter) => boolean} FilterPredicate */
+  /** @typedef {(item: ItemAdapter, nextItem: ItemAdapter) => number} SortCompare */
+
+  /** @type {FilterPredicate} */
+  #filter = () => true;
+
+  /** @type {SortCompare} */
+  #sort = () => 0;
+
+  /** Получит callback фильтра */
+  getFilter() {
+    return this.#filter;
+  }
+
+  /**
+   * Установит фильтр
+   * @param {FilterPredicate} predicate
+   */
+  setFilter(predicate) {
+    this.#filter = predicate;
+
+    this.dispatchEvent(new CustomEvent('filter'));
+
+    return this;
+  }
+
+  /** Получит callback сортировки */
+  getSort() {
+    return this.#sort;
+  }
+
+  /**
+   * Установит сортировку
+   * @param {SortCompare} compare
+   */
+  setSort(compare) {
+    this.#sort = compare;
+
+    this.dispatchEvent(new CustomEvent('sort'));
+
+    return this;
+  }
+
+  list(predicate = this.getFilter(), compare = this.getSort()) {
+    return this.listAll().filter(predicate).sort(compare);
+  }
+}
