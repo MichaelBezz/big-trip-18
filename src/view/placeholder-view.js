247cef30db98c535c46be1868a622794ac42d4f9
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-09-07 11:40:44 +0300

    Добавит placeholder-view

---
+++ src/view/placeholder-view.js
@@ -0,0 +1,24 @@
+import ComponentView, {html} from './component-view.js';
+
+/** Представление placeholder */
+export default class PlaceholderView extends ComponentView {
+
+  /** @override */
+  createAdjacentHtml() {
+    return html`
+      <p class="trip-events__msg"></p>
+    `;
+  }
+
+  /**
+   * Установит placeholder
+   * @param {string} placeholder
+   */
+  setPlaceholder(placeholder) {
+    this.querySelector('.trip-events__msg').textContent = placeholder;
+
+    return this;
+  }
+}
+
+customElements.define(String(PlaceholderView), PlaceholderView);
