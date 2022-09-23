21a74d57a7ec9bd89581f6733b823cba83e662d3
Michael Bezzaponnyy <michael.bezzaponnyy@gmail.com>
2022-09-23 19:00:56 +0300

    Добавит блокировку клавиатуры

--- src/view/loader-view.js
+++ src/view/loader-view.js
@@ -5,18 +5,34 @@ export default class LoaderView extends View {
   constructor() {
     super();
 
+    this.rootView = document.body;
+
     this.classList.add('loader');
   }
 
   /**
    * @override
    * @param {boolean} flag
-   * @param {HTMLElement} rootView
    */
-  display(flag, rootView = document.body) {
-    rootView[flag ? 'append' : 'removeChild'](this);
+  display(flag) {
+    this.rootView[flag ? 'append' : 'removeChild'](this);
 
     return this;
+  }
+
+  connectedCallback() {
+    this.rootView.addEventListener('keydown', this);
+  }
+
+  disconnectedCallback() {
+    this.rootView.removeEventListener('keydown', this);
+  }
+
+  /**
+   * @param {KeyboardEvent} event
+   */
+  handleEvent(event) {
+    event.preventDefault();
   }
 }
 
